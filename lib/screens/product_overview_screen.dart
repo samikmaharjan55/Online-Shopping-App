76c1b4099fde266c78d34638bbbff8205a7b91aa
Samik Maharjan <samikmaharjan55@gmail.com>
2022-03-23 21:22:34 +0545

    Updated

---
+++ lib/screens/product_overview_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:online_shopping_app/widgets/product_item.dart';
+
+class ProductOverviewScreen extends StatefulWidget {
+  const ProductOverviewScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProductOverviewScreen> createState() => _ProductOverviewScreenState();
+}
+
+class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shop Venue'),
+      ),
+      body: GridView.builder(
+          itemCount: 4,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            childAspectRatio: 3 / 2,
+          ),
+          itemBuilder: (ctx, index) => ProductItem()),
+    );
+  }
+}
